0dbdcdd30445b8919424f8bb468f5cbcd821eb96
ashutosh <adityatripathi1011@gmail.com>
2022-06-30 16:01:39 +0530

    closure

--- part9/closures.js
+++ part9/closures.js
@@ -1,2 +1,64 @@
-'use strict'
+"use strict";
 
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+//closure makes remember all the variables existed at function birthplace
+
+console.dir(booker); // [Function (anonymous)]
+
+// more examples
+
+//1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 111;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+// re-assigning function
+h();
+f();
+console.dir(f);
+
+// exmaple -2
+
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`we are now boarding all ${n}`);
+    console.log(`there are three group with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`will start baord in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//   console.log("TIMER");
+// }, 3000);
+const perGroup = 1000;
+
+boardPassengers(100, 3);
